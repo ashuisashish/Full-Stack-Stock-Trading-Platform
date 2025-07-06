dc7d7e949fd8527500f054c9473ff08534a69a27
Ashish Singh <ashuisashish333@gmail.com>
2025-07-06 21:05:16 +0530

    project structure prepared

---
+++ frontend/src/landing_page/products/Hero.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Hero() {
+    return ( 
+        <h1>Hero Product</h1>
+     );
+}
+
+export default Hero;
---
+++ frontend/src/landing_page/products/Universe.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Universe() {
+    return ( 
+        <h1>Product Universe</h1>
+     );
+}
+
+export default Universe;
