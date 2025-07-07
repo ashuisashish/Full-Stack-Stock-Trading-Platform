9809a5178071381531771a8d159b30b4fcbbc6a1
Ashish Singh <ashuisashish333@gmail.com>
2025-07-07 08:11:39 +0530

    correction done

--- frontend/src/landing_page/home/Hero.js
+++ frontend/src/landing_page/home/Hero.js
@@ -7,7 +7,7 @@ function Hero() {
                 <img src="media/images/homeHero.png" alt="Hero-Image" className='mb-5' />
                 <h1 className='mt-5'>Invest in everything</h1>
                 <p>Online platform to invest in stocks, derivatives, mutual funds, and more</p>
-                <button className='p-2 btn btn-primary fs-5 ' style={{width:"20%", margin:"0 auto"}}>Signup now</button>
+                <button className='p-2 btn btn-primary fs-5 ' style={{width:"18%", margin:"0 auto"}}>Signup now</button>
                 
             </div>
         </div>
