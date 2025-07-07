3bb053e0663a1179fcc677270a581cae86fac54b
Ashish Singh <ashuisashish333@gmail.com>
2025-07-07 08:00:18 +0530

    images uploaded

--- frontend/src/landing_page/home/Hero.js
+++ frontend/src/landing_page/home/Hero.js
@@ -2,7 +2,15 @@ import React from 'react'
 
 function Hero() {
     return ( 
-        <h1>Hero</h1>
+        <div className='container'>
+            <div className="row">
+                <img src="media/images/homeHero.png" alt="Hero Image" />
+                <h1>Invest in everything</h1>
+                <p>Online platform to invest in stocks, derivatives, mutual funds, and more</p>
+                <button>Signup now</button>
+                
+            </div>
+        </div>
      );
 }
 
