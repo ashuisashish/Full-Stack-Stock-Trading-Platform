62e9972b832fa9045e3c7fc499a574f6a05bb5e6
Ashish Singh <ashuisashish333@gmail.com>
2025-07-07 08:10:10 +0530

    hero section is done

--- frontend/src/landing_page/home/Hero.js
+++ frontend/src/landing_page/home/Hero.js
@@ -2,12 +2,12 @@ import React from 'react'
 
 function Hero() {
     return ( 
-        <div className='container'>
-            <div className="row">
-                <img src="media/images/homeHero.png" alt="Hero Image" />
-                <h1>Invest in everything</h1>
+        <div className='container p-5'>
+            <div className="row text-center" >
+                <img src="media/images/homeHero.png" alt="Hero-Image" className='mb-5' />
+                <h1 className='mt-5'>Invest in everything</h1>
                 <p>Online platform to invest in stocks, derivatives, mutual funds, and more</p>
-                <button>Signup now</button>
+                <button className='p-2 btn btn-primary fs-5 ' style={{width:"20%", margin:"0 auto"}}>Signup now</button>
                 
             </div>
         </div>
